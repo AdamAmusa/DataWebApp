6b854146b42581916283af9c590241682530ba1e
Adam Amusa <Adamamusa2004@gmail.com>
2023-12-13 16:04:16 +0000

    stores page reads data from mysql

---
+++ DAO.js
@@ -0,0 +1,37 @@
+var pool;
+var mySQL = require('promise-mysql')
+
+mySQL.createPool({
+    connectionLimit: 3,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'proj2023'
+})
+    .then(p => {
+        pool = p
+    })
+    .catch(e => {
+        console.log("pool error:" + e)
+    })
+
+
+
+var getStores = function () {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * FROM store')
+            .then((data) => {
+                resolve(data)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+
+
+}
+
+
+
+module.exports = {getStores};
+
---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var app = express()
+var port = 3000;
+var index = require('./DAO.js')
+
+app.set('view engine', 'ejs');
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+
+
+app.get('/', (req, res) => {
+    res.render("home");
+})
+
+
+app.get('/stores', (req, res) => {
+    index.getStores()
+        .then((data) => {
+            console.log("OK =========")
+            res.render("stores", { "store": data })
+        })
+        .catch(error => {
+            console.log("ERROR ========")
+            res.send(error)
+        })
+})
+
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
+})
